8a86ccad6b7ab2a55374882577f6373d83a3e2ab
Omina Elsheikh <ominaelsheikh94@gmail.com>
2026-09-14 00:12:52 -0400

    refactoring phase 1

--- main.cpp
+++ main.cpp
@@ -1,39 +1,70 @@
 #include <iostream>
 
-double getcpuUsage(){
+struct SystemStats
+{
+    double cpuUsage{};
+    double memUsage{};
+    int numProcesses{};
+
+};
+
+
+double getCpuUsage(){
     double usage{};
     std::cin >> usage;
     return usage;
 
 }
-double getmemUsage(){
+double getMemUsage(){
     double memory{};
     std::cin >> memory;
     return memory;
 
 }
 
-int getnumProcesses(){
+int getNumProcesses(){
     int processes{};
     std::cin >> processes;
     return processes;
 }
 
+SystemStats collectSystemStats(){
+    SystemStats stats {getCpuUsage(),getMemUsage(),getNumProcesses()};
+    return stats;
+
+}
+
+void displayStats(const SystemStats& stats){
+    std::cout << "System Stats" << '\n';
+    std::cout << "CPU Usage:   " << stats.cpuUsage << '\n';
+    std::cout << "Memory Usage:  " << stats.memUsage << '\n';
+    std::cout << "Processes: " << stats.numProcesses << '\n';
+}
+
+void checkWarnings(const SystemStats& stats)
+{
+    const double cpuUsageWarningThreshold{80.0};
+    const double memUsageWarningThreshold{80.0};
+
+    if (stats.cpuUsage >= cpuUsageWarningThreshold)
+    {
+        std::cout << "WARNING: High CPU usage!\n";
+    }
+
+    if (stats.memUsage >= memUsageWarningThreshold)
+    {
+        std::cout << "WARNING: High memory usage!\n";
+    }
+
+
+}
+
 int main(){
-    //const double cpuUsageWarningThreshold{80.0};
-    //const double memUsageWarningThreshold{80.0};
+    std::cout << "Enter system stats: ";
+    SystemStats stats{collectSystemStats()};
 
-    std::cout << "Please enter cpu usage: ";
-    double cpUsage{getcpuUsage()};
-    std::cout << "CPU Usage: " << cpUsage << "\n";
-
-    std::cout << "Please enter memory usage: ";
-    double memUsage{getmemUsage()};
-    std::cout << "Memory Usage: " << memUsage << "\n";
-    
-    std::cout << "Please enter number of processes: ";
-    int proc{getnumProcesses()};
-    std::cout << "Processes: " << proc << "\n";
+    displayStats(stats);
+    checkWarnings(stats);
 
     return 0;
 }
